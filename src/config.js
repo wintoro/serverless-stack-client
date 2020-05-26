592cef4aceb1fac1d7cd68ae3111952cec2c89d7
Wintoro <wint.design@gmail.com>
2020-05-26 13:40:52 +0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "wint-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://x2ko94ez96.execute-api.us-east-1.amazonaws.com/prod/"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_siNRKhZl7",
+        APP_CLIENT_ID: "3mt7b8dt51vbk1vrjm50nml9jj",
+        IDENTITY_POOL_ID: "us-east-1:7ac7cc22-2a2f-4573-bc65-8995b9619ae5"
+    }
+};
